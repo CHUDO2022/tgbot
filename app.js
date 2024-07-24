372bca4b932dcb4f58635faff6edb2e384a9cb5c
CHUDO2022 <102815524+CHUDO2022@users.noreply.github.com>
2024-07-25 00:10:55 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelectorAll('.btn');
     const userCard = document.getElementById('usercard');
+    const telegram = window.Telegram.WebApp;
 
     buttons.forEach(button => {
         button.addEventListener('click', () => {
             const productId = button.id.replace('btn', ''); // Извлекаем номер товара из id кнопки
-            userCard.textContent = `Вы выбрали такой товар №${productId}`;
+            const message = `Вы выбрали такой товар №${productId}`;
+            userCard.textContent = message;
+
+            // Отправляем данные в Telegram бот
+            telegram.sendData(JSON.stringify({ productId: productId, message: message }));
         });
     });
 
     const closeBtn = document.querySelector('.close-btn');
     closeBtn.addEventListener('click', () => {
-        Telegram.WebApp.close();
+        telegram.close();
     });
 });
