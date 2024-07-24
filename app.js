82125ae348d748e99ce6c80dcba5364bdbaad414
CHUDO2022 <102815524+CHUDO2022@users.noreply.github.com>
2024-07-24 21:11:45 +0500

    new app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ tg.MainButton.color = '#2cab37';
 
 let item = "";
 
+// Функция для логирования и отправки данных
 function logAndSendClick(item) {
     let timestamp = new Date().toISOString();
     let logData = {
@@ -24,95 +25,49 @@ function logAndSendClick(item) {
     tg.sendData(JSON.stringify(logData));
 }
 
-let btn1 = document.getElementById("btn1");
-let btn2 = document.getElementById("btn2");
-let btn3 = document.getElementById("btn3");
-let btn4 = document.getElementById("btn4");
-let btn5 = document.getElementById("btn5");
-let btn6 = document.getElementById("btn6");
+// Функция для обработки кликов по кнопкам
+function setupButton(buttonId, itemNumber) {
+    let button = document.getElementById(buttonId);
+    button.addEventListener("click", function() {
+        if (tg.MainButton.isVisible) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.setText(`Вы выбрали товар ${itemNumber}!`);
+            item = itemNumber;
+            tg.MainButton.show();
+            logAndSendClick(item);
+        }
+    });
+}
 
-btn1.addEventListener("click", function() {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    } else {
-        tg.MainButton.setText("Вы выбрали товар 1!");
-        item = "1";
-        tg.MainButton.show();
-        logAndSendClick(item);
-    }
-});
+// Настройка всех кнопок
+setupButton("btn1", "1");
+setupButton("btn2", "2");
+setupButton("btn3", "3");
+setupButton("btn4", "4");
+setupButton("btn5", "5");
+setupButton("btn6", "6");
 
-btn2.addEventListener("click", function() {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    } else {
-        tg.MainButton.setText("Вы выбрали товар 2!");
-        item = "2";
-        tg.MainButton.show();
-        logAndSendClick(item);
-    }
-});
-
-btn3.addEventListener("click", function() {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    } else {
-        tg.MainButton.setText("Вы выбрали товар 3!");
-        item = "3";
-        tg.MainButton.show();
-        logAndSendClick(item);
-    }
-});
-
-btn4.addEventListener("click", function() {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    } else {
-        tg.MainButton.setText("Вы выбрали товар 4!");
-        item = "4";
-        tg.MainButton.show();
-        logAndSendClick(item);
-    }
-});
-
-btn5.addEventListener("click", function() {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    } else {
-        tg.MainButton.setText("Вы выбрали товар 5!");
-        item = "5";
-        tg.MainButton.show();
-        logAndSendClick(item);
-    }
-});
-
-btn6.addEventListener("click", function() {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    } else {
-        tg.MainButton.setText("Вы выбрали товар 6!");
-        item = "6";
-        tg.MainButton.show();
-        logAndSendClick(item);
-    }
-});
-
+// Обработка клика по основной кнопке Telegram
 Telegram.WebApp.onEvent("mainButtonClicked", function() {
     tg.sendData(item);
 });
 
+// Отображение информации о пользователе
 let usercard = document.getElementById("usercard");
 
-let userImage = document.createElement("img");
-userImage.src = tg.initDataUnsafe.user.photo_url;
-userImage.alt = "User Photo";
-userImage.style.borderRadius = "50%";
-userImage.style.width = "100px";
-userImage.style.height = "100px";
-userImage.style.objectFit = "cover";
+if (tg.initDataUnsafe.user) {
+    let userImage = document.createElement("img");
+    userImage.src = tg.initDataUnsafe.user.photo_url || 'default-avatar.png'; // Поставьте здесь URL по умолчанию, если нет фото
+    userImage.alt = "User Photo";
+    userImage.style.borderRadius = "50%";
+    userImage.style.width = "100px";
+    userImage.style.height = "100px";
+    userImage.style.objectFit = "cover";
 
-let userName = document.createElement("p");
-userName.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
+    let userName = document.createElement("p");
+    userName.innerText = `${tg.initDataUnsafe.user.first_name || ''} ${tg.initDataUnsafe.user.last_name || ''}`;
 
-usercard.appendChild(userImage);
-usercard.appendChild(userName);
+    usercard.appendChild(userImage);
+    usercard.appendChild(userName);
+}
